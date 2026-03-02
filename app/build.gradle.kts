508725a5a824162d02f4f20c74768791820c5fd7
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-03-02 11:01:30 +0100

    update kotlin 2.3.10 and compose 2026.02.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation(libs.androidx.material.icons.extended)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
