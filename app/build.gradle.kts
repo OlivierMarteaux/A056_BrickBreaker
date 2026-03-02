b4bf2e533db870092a8cf1243751de695275dd91
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-03-02 11:17:45 +0100

    Removed data/User.kt, updated navigation.compose and lifecycle.viewmodel.compose versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     
-    implementation("androidx.navigation:navigation-compose:2.8.5")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.material.icons.extended)
 
     testImplementation(libs.junit)
